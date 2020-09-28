79940040a102084989e9ccdab59a851b683815e6
Chris Blinde <chrisblinde@gmail.com>
2020-09-28 09:49:34 -0400

    Add project files.

---
+++ ShowTrackerMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShowTrackerMVC.Startup))]
+namespace ShowTrackerMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
